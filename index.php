15aa10bea07eb418d9b57379a4b1747ed7ffb548
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2023-11-01 18:20:13 +0200

    1.11.18:20

--- index.php
+++ index.php
@@ -3,10 +3,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ETUSIVU</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <header>
         <h1>OTSIKKO</h1>
+
+        <!--NAVIGOINTI-->
+        <nav>
+            <a href=""><img src="images/menu.png"></a>
+        </nav>
+
+        <!--SEARCHBAR-->
+        <div class="search-container">
+            <input type="text" placeholder="Etsi tuotteita">
+            <button>Etsi</button>
+        </div>
+
     </header>
     <main>
     </main>
