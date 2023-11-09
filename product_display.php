aadf34fc9ad755b11ab98e55f4c287ba0e717e96
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2023-11-09 13:28:47 +0200

    9.11.13:28

--- product_display.php
+++ product_display.php
@@ -1,10 +1,84 @@
+<?php
+    // Tarkistetaan, onko ProductID asetettu URL-parametreihin
+   // if (isset($_GET['ProductID'])){
+
+    // Tietokanta yhteys
+    // Palvelimen nimi muuttujaan
+    $servername = "localhost";
+    $databasename = "verkkokauppa";
+    $username = "root";
+    $password = "";
+    
+    //Yritetään
+    try {
+        //Luodaan yhteys, joka on PDO objekti
+        $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+
+        //PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $productID = $_GET['ProductID'];    //Haetaan URL:stä oleva ProductID muuttujaan
+        $products_kysely = $conn->prepare("SELECT * FROM products WHERE ProductID = :productID");
+        $products_kysely->bindParam(':productID', $productID);
+        $products_kysely->execute();
+
+        $product = $products_kysely->fetch();
+    }
+    catch (PDOException $e) {
+        // Yhteys epäonnistui
+        echo "". $e->getMessage();
+    }
+//} echo "<p>Tuotetta ei löytynyt!</p>";  //URL:ssä ei ollut ProductID:tä joten siitä virheilmoitus
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>KIRJA-SOPPI - ETUSIVU</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    Tuotteita
+    <header>
+        <!--BANNERI & SEARCH-->
+        <div class="banner-search-container">
+            <!--BANNERI-->
+            <div class="banner">
+                <h1>TUOTE-SOPPI</h1>
+            </div>
+        
+            <!--SEARCHBAR-->
+            <div class="search-container">
+                <input type="text" placeholder="Etsi tuotteita">
+                <button>Etsi</button>
+            </div>
+        </div>
+        
+        <!--MENU & KIRJAUTUMINEN & OSTOSKORI-->
+        <div class="menu-log-cart-container">
+            <div class="menu">
+                <a href=""><img src="images/menutext.png"></a>
+            </div>
+            <div class="log">
+                <a href="login.php"><img src="images/logtext.png"></a>
+            </div>
+            <div class="cart">
+                <a href=""><img src="images/carttext.png"></a>
+            </div>
+        </div>
+    </header>
+
+    <div class="index-main">                                                                  
+    <?php
+                echo "<div class='product-container'>";
+                    echo "<h2>" . $product["ProductName"] . "</h2>";
+                    echo "<p>" . $product["Description"] . "</p>";
+                    echo "<p>Hinta: €" . $product["Price"] . "</p>";
+                    echo "<img src=product_images/". $product["ImageURL"] .">";
+                echo "</div>";
+    ?>
+    </div>
+    <footer>
+    </footer>
 </body>
 </html>
--- products.php
+++ products.php
@@ -1,13 +1,11 @@
 <?php
-    session_start();
-
     // Tietokanta yhteys
-    // Palvelin, tietokanta, tunnukset palvelimelle (user, password)
+    // Palvelimen nimi muuttujaan
     $servername = "localhost";
     $databasename = "verkkokauppa";
     $username = "root";
     $password = "";
-
+    
     //Yritetään
     try {
         //Luodaan yhteys, joka on PDO objekti
@@ -16,21 +14,13 @@
         //PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        //Valmistellaan kysely
-        $users_kysely = $conn->prepare("SELECT * FROM users");
-        $products_kysely = $conn->prepare("SELECT * FROM products");
-        $orders_kysely = $conn->prepare("SELECT * FROM orders");
-        $order_items_kysely = $conn->prepare("SELECT * FROM orderitems");
+        $products_kysely = $conn->prepare("SELECT * FROM products"   );
 
-        //Suoritetaan kysely
-        $users_kysely->execute();
         $products_kysely->execute();
-        $orders_kysely->execute();
-        $order_items_kysely->execute();
     }
     catch (PDOException $e) {
         // Yhteys epäonnistui
-        echo "Connection failed". $e->getMessage();
+        echo "". $e->getMessage();
     }
 ?>
 
@@ -38,93 +28,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>KIRJA-SOPPI - ETUSIVU</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-<h2>Users</h2>
-<table border="1">
-    <tr>
-        <th>User ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Address</th>
-        <th>User Type</th>
-    </tr>
-        <?php
-            while($rivi = $users_kysely->fetch()) {
-                echo "<tr>";
-                echo "<td>" . $rivi["UserID"] . "</td>";
-                echo "<td>" . $rivi["FirstName"] . "</td>";
-                echo "<td>" . $rivi["LastName"] . "</td>";
-                echo "<td>" . $rivi["Email"] . "</td>";
-                echo "<td>" . $rivi["Address"] . "</td>";
-                echo "<td>" . $rivi["UserType"] . "</td>";
-            }
-        ?>
-</table>
+    <header>
+        <!--BANNERI & SEARCH-->
+        <div class="banner-search-container">
+            <!--BANNERI-->
+            <div class="banner">
+                <h1>TUOTE-SOPPI</h1>
+            </div>
+        
+            <!--SEARCHBAR-->
+            <div class="search-container">
+                <input type="text" placeholder="Etsi tuotteita">
+                <button>Etsi</button>
+            </div>
+        </div>
+        
+        <!--MENU & KIRJAUTUMINEN & OSTOSKORI-->
+        <div class="menu-log-cart-container">
+            <div class="menu">
+                <a href=""><img src="images/menutext.png"></a>
+            </div>
+            <div class="log">
+                <a href="login.php"><img src="images/logtext.png"></a>
+            </div>
+            <div class="cart">
+                <a href=""><img src="images/carttext.png"></a>
+            </div>
+        </div>
+    </header>
 
-    <h2>Products</h2>
-<table border="1">
-    <tr>
-        <th>Product ID</th>
-        <th>Product Name</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th>Image</th>
-    </tr>
+    <!--TUOTTEITA KUUSI KAPPALETTA, EI VIELÄ RANDOMILLA-->
+    <div class="index-main">                                                                  
     <?php
             while($rivi = $products_kysely->fetch()) {
-                echo "<tr>";
-                echo "<td>" . $rivi["ProductID"] . "</td>";
-                echo "<td>" . $rivi["ProductName"] . "</td>";
-                echo "<td>" . $rivi["Description"] . "</td>";
-                echo "<td>" . $rivi["Price"] . "</td>";
-                //echo "<td>" . $rivi["Image"] . "</td>";
+                echo "<div class='product-container'>";
+                    echo "<h3>" . $rivi["ProductName"] . "</h3>";
+                    echo "<a href='product_display.php?ProductID=" . $rivi["ProductID"]. "'><img src=product_images/". $rivi["ImageURL"] . "></a>";
+                    echo "<p>" . $rivi["Price"] . "</p>";
+                echo "</div>";
             }
         ?>
-</table>
-
-<h2>Orders</h2>
-<table border="1">
-    <tr>
-        <th>Order ID</th>
-        <th>User ID</th>
-        <th>Order Date</th>
-        <th>Status</th>
-        <th>Total Price</th>
-    </tr>
-    <?php
-            while($rivi = $orders_kysely->fetch()) {
-                echo "<tr>";
-                echo "<td>" . $rivi["OrderID"] . "</td>";
-                echo "<td>" . $rivi["UserID"] . "</td>";
-                echo "<td>" . $rivi["OrderDate"] . "</td>";
-                echo "<td>" . $rivi["Status"] . "</td>";
-                echo "<td>" . $rivi["TotalPrice"] . "</td>";
-            }
-        ?>
-</table>
-
-<h2>Order Items</h2>
-<table border="1">
-    <tr>
-        <th>Order Item ID</th>
-        <th>Order ID</th>
-        <th>Product ID</th>
-        <th>Quantity</th>
-        <th>Subtotal</th>
-    </tr>
-    <?php
-            while($rivi = $order_items_kysely->fetch()) {
-                echo "<tr>";
-                echo "<td>" . $rivi["OrderItemID"] . "</td>";
-                echo "<td>" . $rivi["OrderID"] . "</td>";
-                echo "<td>" . $rivi["ProductID"] . "</td>";
-                echo "<td>" . $rivi["Quantity"] . "</td>";
-                echo "<td>" . $rivi["Subtotal"] . "</td>";
-            }
-        ?>
-</table>
+    </div>
+    <footer>
+    </footer>
 </body>
 </html>
